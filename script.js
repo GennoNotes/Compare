b117f824feb2cbcf8b867407c5a7bca22e7c947e
GennoNotes <51422740+GennoNotes@users.noreply.github.com>
2026-01-28 14:20:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -345,14 +345,14 @@
       block.className = "block";
 
       if (step.type === "insertB") {
-        block.appendChild(makeTitle('Inserted page in ${fileB.name}: Page ${step.bIndex + 1}', true));
+        block.appendChild(makeTitle('Inserted page in ${fileB.name}: Page ${step.bIndex + 1}'));
         block.appendChild(makeMeta('This page exists only in ${fileB.name}'));
         results.appendChild(block);
         continue;
       }
 
       if (step.type === "deleteA") {
-        block.appendChild(makeTitle('Removed from ${fileB.name} (exists in ${fileA.name}): Page ${step.aIndex + 1}', true));
+        block.appendChild(makeTitle('Removed from ${fileB.name} (exists in ${fileA.name}): Page ${step.aIndex + 1}'));
         block.appendChild(makeMeta('This page exists only in ${fileA.name}'));
         results.appendChild(block);
         continue;
